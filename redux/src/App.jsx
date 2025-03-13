95ff747da086c2a8af9e3971d34f376d783f7379
patelSahil9 <ptulasi578@gmail.com>
2025-03-13 16:34:12 +0530

    .

--- redux/src/App.jsx
+++ redux/src/App.jsx
@@ -1,16 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { increment } from './features/counter/CounterSlice'
+import { decrement } from './features/counter/CounterSlice'
+import { reset } from './features/counter/CounterSlice'
+import { incrementByAmount } from './features/counter/CounterSlice'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const dispatch = useDispatch()
+  const count = useSelector((state) => state.counter.value);
+  const [amount, setAmount] = useState(0)
+
+  function handleIncrementClick(){
+    dispatch(increment())
+  }
+  function handleDecrementClick(){
+    dispatch(decrement())
+  }
+  function handleResetClick(){
+    dispatch(reset())
+  }
+  function handleIncrementByAmountClick(){
+    dispatch(incrementByAmount(amount))
+  }
 
   return (
     <>
-      <div>
-        <h1>helloo jii</h1>
-      </div>
+    <div className='container'>
+      <button onClick={handleIncrementClick}>Pluce</button> 
+      <p>Count: {count}</p>
+      <button onClick={handleDecrementClick}>Minuce</button> 
+      <br />
+      <br />
+      <button onClick={handleResetClick}>Recat</button> 
+      <br />
+      <br />
+      <input type='number'
+      placeholder='Enter Amount'
+      value={amount}
+      onChange={(e) => setAmount(e.target.value)}/>
+      <br/>
+      <button onClick={() => dispatch(handleIncrementByAmountClick(amount))}>Increment By Amount</button>
+    </div>
     </>
   )
 }
